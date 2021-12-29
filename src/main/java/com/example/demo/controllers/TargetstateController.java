e60d921fc714a6770a53f2e5ff222de0eb29d410
DanielHochgatterer <dani060798@gmx.at>
2021-12-29 16:52:01 +0100

    Fully commented TargetstateController

--- src/main/java/com/example/demo/controllers/TargetstateController.java
+++ src/main/java/com/example/demo/controllers/TargetstateController.java
@@ -21,87 +21,94 @@ public class TargetstateController {
         this.repository = repository;
     }
 
-    //gibt alle departments zurück
+    //Gibt alle Targetstates mit allen Variablen als Liste zurück. Kann für darstellung in Tabellen verwendet werden.
     @GetMapping("/Targetstate")
     List<TargetstateEntity> all() {
         return repository.findAll();
     }
 
-    //Getter für active-Feld
+    //Folgende Get Mappings sind für alle Variablen eines bestimmten Targetstates. Ansprechbar ist jedes mapping über den grünen Pfad als HTTP Request
+    //Jede Variable des Targetstates ist abgedeckt. Gewisse Variablen wie last_updated, active_since oder last synced generieren sich in post/patch Mapping von selbst da sie für Zeitpunkte oder ein Datum verwendet werden.
     @GetMapping("Targetstate/active/{active}")
     List <TargetstateEntity> withTargetstateActive(@PathVariable int active) {
         return repository.findByActive(active);
     }
 
+    //Getter für targetConfigID Feld
     @GetMapping("Targetstate/targetconfigid/{targetconfigid}")
     List<TargetstateEntity> withTargetstateTargetconfig_id(@PathVariable int targetconfigid) {
         return repository.findByTargetconfigid(targetconfigid);
     }
 
-
-
-    //Getter für active-Feld
+    //Getter für lastupdated Feld
     @GetMapping("Targetstate/Date/last_updated/{lastupdated}")
     List <TargetstateEntity> withTargetstateLastUpdated(@PathVariable Date lastupdated) {
         return repository.findByLastupdated(lastupdated);
     }
 
-    //Getter für active-Feld
+    //Getter für active-since-Feld
     @GetMapping("Targetstate/Date/active_since/{activesince}")
     List <TargetstateEntity> withTargetstateActiveSince(@PathVariable Date activesince) {
         return repository.findByActivesince(activesince);
     }
-
+    //Getter für targetstateID Feld
     @GetMapping("Targetstate/id/{targetstateid}")
     TargetstateEntity withTargetstateId(@PathVariable int targetstateid) {
         return repository.findByTargetstateid(targetstateid);
     }
 
-    //Getter für targetConfig-Feld
+    //Getter für departmentID Feld
     @GetMapping("Targetstate/department_id/{departmentid}")
     List <TargetstateEntity> withTargetstateDepartmentId(@PathVariable int departmentid) {
         return repository.findByDepartmentid(departmentid);
     }
+    //Getter für TYPE (CSV; MYSQL oder LDAP) Feld
     @GetMapping("Targetstate/type/{type}")
     List <TargetstateEntity> withTargetstateType(@PathVariable Type type) {
         return repository.findByType(type);
     }
 
+    //Getter für PORT Feld
     @GetMapping("Targetstate/port/{port}")
     List <TargetstateEntity> withTargetstatePort(@PathVariable String port) {
         return repository.findByPort(port);
     }
 
+    //Getter für Path Feld, hier steht der Pfad des Targetstates drin.
     @GetMapping("Targetstate/path/{path}")
     List <TargetstateEntity> withTargetstatePath(@PathVariable String path) {
         return repository.findByPath(path);
     }
-
+    //Getter für Usernamen Feld
     @GetMapping("Targetstate/user/{user}")
     List <TargetstateEntity> withTargetstateUser(@PathVariable String user) {
         return repository.findByUser(user);
     }
 
+
+    //Getter für passwort des Usernamen
     @GetMapping("Targetstate/password/{password}")
     List <TargetstateEntity> withTargetstatePassword(@PathVariable String password) {
         return repository.findByPassword(password);
     }
 
+    //Getter für den Hostnamen
     @GetMapping("Targetstate/hostname/{hostname}")
     List <TargetstateEntity> withTargetstateHostname(@PathVariable String hostname) {
         return repository.findByHostname(hostname);
     }
 
+    //Getter für Tabellennamen Feld
     @GetMapping("Targetstate/tablename/{tablename}")
     List <TargetstateEntity> withTargetstateTablename(@PathVariable String tablename) {
         return repository.findByTablename(tablename);
     }
-
+    //Getter für last_sync Feld. Wert generiert sich automatisch in den Post/Patchmethoden
     @GetMapping("Targetstate/Timestamp/last_synced/{lastsynced}")
     List <TargetstateEntity> withTargetstateLastUpdated(@PathVariable Timestamp lastsynced) {
         return repository.findByLastsynced(lastsynced);
     }
-
+    //Mapping für das Erstellen eines neuen Targetstates. Active, Active since, last updatedm last synced generieren sich mit dem jeweiligen Datum/Timestamp automatisch und müssen nicht übergeben werden.
     @PostMapping("/Targetstate")
     TargetstateEntity newTargetstateEntity(@RequestBody TargetstateEntity targetstateEntity) {
         if(ObjectUtils.isEmpty(targetstateEntity.getTargetstateid())) {
@@ -148,6 +155,7 @@ public class TargetstateController {
     }
 
     //PatchMapping = Updaten oder Put Mapping(komplett ersetzen jedes Feld wird neu angelegt)
+    //Mit diesem Mapping können bestehende Targetstates über ihre Targetstate ID abgeändert werden. Autogenerierende werte wie Lastsynced oder last_updated generieren sich hierbei wieder automatisch je nach Zeit/Datum bei einer Änderung.
     @PatchMapping("/Targetstate/{id}")
     public TargetstateEntity patchTargetstate(@PathVariable int id, @RequestBody TargetstateEntity newEmployeeData) {
         TargetstateEntity emp = repository.findByTargetstateid(id);
@@ -172,17 +180,19 @@ public class TargetstateController {
         repository.save(emp);
         return emp;
     }
-
+    //Löschen von Targetstates über Ihre ID
     @DeleteMapping("/Targetstate/{targetstateId}")
     void deleteTargetstate(@PathVariable int targetstateId) {
         repository.deleteById(targetstateId);
     }
 
+    //Löschen aller Targetstates
     @DeleteMapping("/Targetstate")
     void deleteAllTargetstate() {
         repository.deleteAll();
     }
 
+    //Löschen aller inactive Targetstates.
     @DeleteMapping("Targetstate/inactive/{active}")
     void deleteInactiveTargetstates(@PathVariable int active) {
         if (active == 1) {
